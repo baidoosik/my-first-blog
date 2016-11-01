30f1ede834b1cba6538c0dd86bf6ebb77dfddb6b
doosik <qoentlr37@naver.com>
2016-11-01 14:30:06 +0900

    login edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login 
 from . import views
+from django.contrib.auth.views import password_change
+from django.contrib.auth.views import password_change_done
 
 
 urlpatterns = [
@@ -7,11 +10,10 @@ urlpatterns = [
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-	url(r'^login/$','django.contrib.auth.views.login'),
-	url(r'^logout/$',views.logout_page),
-	url(r'^password_change/$','django.contrib.auth.views.password_change',
-		{'post_change_redirect':'/password_change/done/'}),
-	url(r'^password_change/done/$','django.contrib.auth.views.password_change_done'),
+	url(r'^login/$', login, name='login'),
+	url(r'^logout/$',views.logout_page, name='logout_page'),
+	url(r'^password_change/$',password_change, name='password_change'),
+	url(r'^password_change/done/$',password_change_done, name='password_change_done'),
 	url(r'^register/$',views.register_page)
 
 ]
