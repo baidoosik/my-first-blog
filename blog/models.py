dd797584adab6c54515e8a34c421ff13fd83cb81
doosik <qoentlr37@naver.com>
2016-12-02 20:12:41 +0900

    12.2new2

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import image_cropping.fields
 """
 from categories.registration import register_fk
 
