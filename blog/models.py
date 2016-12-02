1880068d1a0a51f1768dc9902df6d900999cbb7d
doosik <qoentlr37@naver.com>
2016-12-02 20:08:52 +0900

    12.2new

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-#from image_cropping import ImageRatioField
 """
 from categories.registration import register_fk
 
