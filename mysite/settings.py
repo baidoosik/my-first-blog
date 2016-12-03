54b2773b20b7b485b19b2595ddc08a506cfe7000
doosik <qoentlr37@naver.com>
2016-12-04 03:57:29 +0900

    12.4 update13

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yy*0p-_ym9fromve=r0hsj02%o4dbja&g@mttmowx##4=@)r)('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [“thankq.pythonanywhere.com",]
+ALLOWED_HOSTS = ["thankq.pythonanywhere.com","localhost:8000",]
 
 
 # Application definition
