9d682a86aaa3368137a9f6291ac54ee525aa9dc0
doosik <qoentlr37@naver.com>
2016-12-04 03:12:00 +0900

    12.4 update13

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 import django.contrib.auth.views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
 	url(r'^accounts/login/$', django.contrib.auth.views.login),
@@ -17,3 +18,11 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^uploads/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
