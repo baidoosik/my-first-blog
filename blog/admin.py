86d0e37a2636308c5cfae4ef984e76337b54b482
doosik <qoentlr37@naver.com>
2016-12-29 22:44:37 +0900

    12.29 post admin 수정

--- blog/admin.py
+++ blog/admin.py
@@ -3,12 +3,14 @@ from .models import Post, Comment, Category, Datestyle1,Datestyle2,Man,Woman,Man
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'title']
+    list_display = ['author', 'category', 'title' ]
     list_display_links =['title']
-
+    list_filter = ['published_date', 'category']
+    search_fields =  ['title']
 
 admin.site.register(Comment)
 admin.site.register(Category)
+#소개팅
 admin.site.register(Datestyle1)
 admin.site.register(Datestyle2)
 admin.site.register(Manstyle)
